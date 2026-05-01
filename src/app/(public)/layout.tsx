1b50d15667be2d353d4035096f0e4105203b48fe
Rafael Miranda <rafonseca@gmail.com>
2026-05-01 11:11:34 -0300

    feat: integrate Google Ads script into public layout
    - Added a Google Ads script to the public layout for enhanced ad management and tracking.
    - Ensured the script loads asynchronously to optimize performance and user experience.

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,19 @@
+import Script from 'next/script'
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1518135456295859"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      {children}
+    </>
+  )
 }
